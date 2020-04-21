74ee90cfb46335235eb5bd5c02705ff339b8143b
Raul <inforagon@gmail.com>
2020-04-21 13:17:38 +0200

    Activate Https

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
+
+const app = express();
 const router = express.Router();
+const PORT = (process.env.PORT || 8080);
+
+const key = fs.readFileSync(__dirname + '/keys/selfsigned.key');
+const cert = fs.readFileSync(__dirname + '/keys/selfsigned.crt');
+var options = {
+    key: key,
+    cert: cert
+};
+
 app.use(express.static(__dirname));
 
 router.get('/', function(req, res) {
@@ -12,9 +24,10 @@ app.all('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
 //add the router
 app.use('/', router);
-app.set('port', (process.env.PORT || 8080));
 
-console.log('Running at Port 8080');
+const server = https.createServer(options, app);
+server.listen(PORT, () => {
+    console.log("server starting on port : " + PORT)
+});
